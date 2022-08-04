05be2c86b044410834f85cf5f43c1ba0ac13587e
EugeneMNS <evgen.str4@gmail.com>
2022-08-04 08:01:16 +0300

    init: todo-input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoInputComponent
   ],
   imports: [
     BrowserModule,
